d7b028d5edfa555a9ec1c92efeb7ea4095dde2bd
Aleksi Polvinen <aleksipolvinen@gmail.com>
2019-12-31 11:43:11 +0200

    remade login screen

--- sm chat/MainWindow.xaml.cs
+++ sm chat/MainWindow.xaml.cs
@@ -24,5 +24,45 @@ namespace sm_chat
         {
             InitializeComponent();
         }
+
+        private void loginbtn_Click(object sender, RoutedEventArgs e)
+        {
+            if(Username.Text!="" && Password.Password!="")
+            {
+
+            }
+        }
+
+        private void Username_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Username_GotKeyboardFocus(object sender, KeyboardFocusChangedEventArgs e)
+        {
+            namelabel.Visibility = Visibility.Hidden;
+        }
+
+        private void Username_LostKeyboardFocus(object sender, KeyboardFocusChangedEventArgs e)
+        {
+            if(Username.Text=="")
+            {
+                namelabel.Visibility = Visibility.Visible;
+            }
+        }
+
+        private void Password_LostKeyboardFocus(object sender, KeyboardFocusChangedEventArgs e)
+        {
+            if (Password.Password == "")
+            {
+                passlabel.Visibility = Visibility.Visible;
+            }
+        }
+
+        private void Password_GotKeyboardFocus(object sender, KeyboardFocusChangedEventArgs e)
+        {
+            passlabel.Visibility = Visibility.Hidden;
+        }
+
     }
 }

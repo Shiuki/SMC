b8ef04e2131859f466be1db1a2588d462454a744
Aleksi Polvinen <aleksipolvinen@gmail.com>
2020-01-06 16:40:22 +0200

    finished settings tab and remade register/login

--- sm chat/MainWindow.xaml.cs
+++ sm chat/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
@@ -41,24 +42,19 @@ namespace sm_chat
             //donot touch this ~nya
             InitializeComponent();
         }
-     
+        string token = "";
+        string name = "";
         private void loginbtn_Click(object sender, RoutedEventArgs e)
         {
-            //sending login request to the api
-            var url = "https://smield.host/SMC_api/index.php";
-            string pars = $"username={Username.Text}&password={Password.Password}";
-
-            using (WebClient wc = new WebClient())
-            {
-                wc.Headers[HttpRequestHeader.ContentType] = "application/x-www-form-urlencoded";
-                Result = wc.UploadString(url, pars);
-            }
-            try
-            {
-                
-                MessageBox.Show(Result);
-            }
-            catch { }
+            login();
+        }
+        public string Between(string STR, string FirstString, string LastString)
+        {
+            string FinalString;
+            int Pos1 = STR.IndexOf(FirstString) + FirstString.Length;
+            int Pos2 = STR.IndexOf(LastString);
+            FinalString = STR.Substring(Pos1, Pos2 - Pos1);
+            return FinalString;
         }
         private void registerbtn_Click(object sender, RoutedEventArgs e)
         {
@@ -112,25 +108,39 @@ namespace sm_chat
         public string wef;
         private void Grid_Loaded(object sender, RoutedEventArgs e)
         {
-            string Uri = $"https://smield.host/SMC_api/ping?{version}";  
+
+            string Uri = $"https://smield.host/SMC_api/ping?{version}";
 
             using (WebClient wc = new WebClient())
             {
                 System.Net.ServicePointManager.SecurityProtocol = System.Net.SecurityProtocolType.Tls | System.Net.SecurityProtocolType.Tls11 | System.Net.SecurityProtocolType.Tls12 | System.Net.SecurityProtocolType.Ssl3;
                 wef = wc.DownloadString(Uri);
             }
-     
+
 
             if (wef.Length > 0 && wef == version)
             {
-               
+                RegistryKey key = Registry.CurrentUser.OpenSubKey(@"SOFTWARE\SMC");
+
+                //if it does exist, retrieve the stored values  
+                if (key != null)
+                {
+                    token = (key.GetValue("token").ToString());
+                    name = (key.GetValue("username").ToString());
+                    key.Close();
+                    if (autologin())
+                    {
+
+                    }
+
+                }
                 //ping successful
                 emaillabel.Visibility = Visibility.Hidden;
                 Email.Visibility = Visibility.Hidden;
                 registerbtn.Visibility = Visibility.Hidden;
                 if (!logged_in)
                 {
-                    //show login screen
+                    //try to login
                     tabControl.SelectedIndex = tabControl.Items.IndexOf(logintab);
                 }
             }
@@ -140,7 +150,7 @@ namespace sm_chat
                 Process.Start("https://smield.host/SMC.exe");
                 this.Close();
             }
-           
+
         }
 
  
@@ -178,6 +188,7 @@ namespace sm_chat
             }
         }
         string Result;
+        string Settings;
 
         private void button_Click(object sender, RoutedEventArgs e)
         {
@@ -204,6 +215,10 @@ namespace sm_chat
         public bool playing = false;
         private void pausebutton_png_MouseDown(object sender, MouseButtonEventArgs e)
         {
+            handleplay();
+        }
+        private void handleplay()
+        {
             if (playing)
             {
                 BitmapImage image = new BitmapImage(new Uri("playbutton.png", UriKind.Relative));
@@ -216,6 +231,183 @@ namespace sm_chat
                 pausebutton_png.Source = image;
                 playing = true;
             }
+        }
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            var url = "https://smield.host/SMC_api/settings.php?";
+          
+            string col = colorfield.Text;
+
+            int music = 0;
+            int startup = 0;
+            if (musicbtn.IsChecked == true)
+            {
+                music = 1;
+            }
+           
+            if (onboot.IsChecked == true)
+            {
+                startup = 1;
+            }
+            string pars = $"email={music}&color={col}&startup={startup}&username={name}&cog={token}&update=yes";
+            using (WebClient wc = new WebClient())
+            {
+                wc.Headers[HttpRequestHeader.ContentType] = "application/x-www-form-urlencoded";
+                Result = wc.UploadString(url, pars);
+            }
+            try
+            {
+
+                MessageBox.Show(Result);
+            }
+            catch { }
+        }
+        private bool autologin()
+        {
+            var url = "https://smield.host/SMC_api/index.php";
+            string pars = $"username={name}&password={token}&usingtoken=true";
+
+            using (WebClient wc = new WebClient())
+            {
+                wc.Headers[HttpRequestHeader.ContentType] = "application/x-www-form-urlencoded";
+                Result = wc.UploadString(url, pars);
+            }
+            try
+            {
+                if (Result.StartsWith("login"))
+                {
+
+                    set_settings();
+                    tabControl.SelectedIndex = tabControl.Items.IndexOf(settingstab);
+                    logged_in = true;
+                    return true;
+                }
+
+            }
+            catch { }
+        
+                return false;
+           
+
+        }
+        private void SetStartup()
+        {
+            RegistryKey rk = Registry.CurrentUser.OpenSubKey
+                ("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
+
+            if (onboot.IsChecked == true)
+                rk.SetValue("SMC",
+
+System.Reflection.Assembly.GetExecutingAssembly().Location);
+            else
+                rk.DeleteValue("SMC", false);
+
+        }
+        private void set_settings()
+        { if (token != "" && name !="")
+            {
+                try
+                {
+                    var url2 = "https://smield.host/SMC_api/settings.php?";
+                    string pars2 = $"username={name}&cog={token}&update=no";
+
+                    using (WebClient wc2 = new WebClient())
+                    {
+                        wc2.Headers[HttpRequestHeader.ContentType] = "application/x-www-form-urlencoded";
+                        Settings = wc2.UploadString(url2, pars2);
+                    }
+                    //MessageBox.Show(Settings);
+
+                    colorfield.Text= Between(Settings, "color: ", " startup");
+                    int music = Convert.ToInt32(Between(Settings, "music: ", " color"));
+                    if(music == 1)
+                    {
+                        musicbtn.IsChecked = true;
+                        playing = true;
+                        handleplay();
+                    }
+                    int startup = Convert.ToInt32(Between(Settings, "startup: ", "?"));
+                    if (startup == 1)
+                    {
+                        onboot.IsChecked = true;
+                        SetStartup();
+                    }
+
+                }
+                catch { }
+            }
+        }
+        private void login()
+        {
+
+
+                //sending login request to the api
+                var url = "https://smield.host/SMC_api/index.php";
+                string pars = $"username={Username.Text}&password={Password.Password}";
+
+                using (WebClient wc = new WebClient())
+                {
+                    wc.Headers[HttpRequestHeader.ContentType] = "application/x-www-form-urlencoded";
+                    Result = wc.UploadString(url, pars);
+                }
+                try
+                {
+
+                    token = Between(Result, "token:", "-");
+                    name = Between(Result, "name:", "?");
+
+                    RegistryKey key = Registry.CurrentUser.CreateSubKey(@"SOFTWARE\SMC");
+
+                    //storing the values  
+                    key.SetValue("token", token);
+                    key.SetValue("username", name);
+                    key.Close();
+                    if (Result.StartsWith("login"))
+                    {
+                        MessageBox.Show("Login successful");
+                    }
+                    else
+                    {
+                        MessageBox.Show(Result);
+                    }
+                }
+
+
+                catch { MessageBox.Show(Result); }
+            
+           
+        }
+
+        private void colorfield_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            try
+            {
+                var converter = new System.Windows.Media.BrushConverter();
+                var brush = (Brush)converter.ConvertFromString(colorfield.Text);
+                colorlaber.Foreground = brush;
+            }
+            catch { }
+        }
+
+        private void onboot_Checked(object sender, RoutedEventArgs e)
+        {
+            SetStartup();
+        }
+
+        private void onboot_Unchecked(object sender, RoutedEventArgs e)
+        {
+            SetStartup();
+        }
+
+        private void button2_Click(object sender, RoutedEventArgs e)
+        {
+            token = "";
+            name = "";
+
+            Registry.CurrentUser.DeleteSubKey(@"SOFTWARE\SMC",false);
+            tabControl.SelectedIndex = tabControl.Items.IndexOf(logintab);
+            logged_in = false;
+
         }
     }
 }

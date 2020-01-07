dd1b8abb16179ece55a1c7066bbe69ef3434eab0
Aleksi Polvinen <aleksipolvinen@gmail.com>
2020-01-07 09:03:48 +0200

    changed logo and fixed radio

--- sm chat/MainWindow.xaml.cs
+++ sm chat/MainWindow.xaml.cs
@@ -6,6 +6,7 @@ using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Net;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Timers;
@@ -360,6 +361,9 @@ System.Reflection.Assembly.GetExecutingAssembly().Location);
                         onboot.IsChecked = true;
                         SetStartup();
                     }
+                    var converter = new System.Windows.Media.BrushConverter();
+                    var brush = (Brush)converter.ConvertFromString(colorfield.Text);
+                    colorlaber.Foreground = brush;
 
                 }
                 catch { }
@@ -446,12 +450,39 @@ System.Reflection.Assembly.GetExecutingAssembly().Location);
         }
         private void getsong(object source, ElapsedEventArgs e)
         {
-            using (WebClient wc = new WebClient())
-            {
-                var json = wc.DownloadString("https://kuronegai.radioca.st/stats?json=1");
+         
+                var json = WebClient_DownLoadString(new Uri("https://kuronegai.radioca.st/stats?json=1", UriKind.Absolute));
                 dynamic data = JObject.Parse(json);
-                Application.Current.Dispatcher.Invoke(new Action(() => songlabel.Content = (data.songtitle)));
-                wc.Dispose();
+                Application.Current.Dispatcher.Invoke(new Action(() => songlabel.Content = (data.songtitle).ToString()));
+              
+            
+        }
+        public string WebClient_DownLoadString(Uri URI)
+        {
+            using (WebClient webclient = new WebClient())
+            {
+                ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+
+                webclient.CachePolicy = new System.Net.Cache.RequestCachePolicy(System.Net.Cache.RequestCacheLevel.BypassCache);
+                webclient.Headers.Add(HttpRequestHeader.Accept, "ext/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8");
+                webclient.Headers.Add(HttpRequestHeader.AcceptLanguage, "en-US,en;q=0.8");
+                webclient.Headers.Add(HttpRequestHeader.KeepAlive, "keep-alive");
+
+                string result = webclient.DownloadString(URI);
+                using (HttpWebResponse wc_response = (HttpWebResponse)webclient
+                                .GetType()
+                                .GetField("m_WebResponse", BindingFlags.Instance | BindingFlags.NonPublic)
+                                .GetValue(webclient))
+                {
+                    Encoding PageEncoding = Encoding.GetEncoding(wc_response.CharacterSet);
+                    byte[] bresult = webclient.Encoding.GetBytes(result);
+                    using (MemoryStream memstream = new MemoryStream(bresult, 0, bresult.Length))
+                    using (StreamReader reader = new StreamReader(memstream, PageEncoding))
+                    {
+                        memstream.Position = 0;
+                        return reader.ReadToEnd();
+                    };
+                };
             }
         }
     }
